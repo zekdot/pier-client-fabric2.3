6415e0a6d42d64b736e08ca27eb889026c865b7e
zekdot <452526076@qq.com>
2022-02-15 21:18:19 +0800

    create a broker-invoked service

---
+++ broker_service/config.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	ConfigName = "fabric.toml"
+)
+
+type Fabric struct {
+	Name        string `toml:"name" json:"name"`
+	Addr        string `toml:"addr" json:"addr"`
+	OrganizationsPath        string `toml:"organizations_path" json:"organizations_path"`
+	Username    string `toml:"username" json:"username"`
+	CCID        string `toml:"ccid" json:"ccid"`
+	ChannelId   string `mapstructure:"channel_id" toml:"channel_id" json:"channel_id"`
+
+}
+
+func DefaultConfig() *Fabric {
+	return &Fabric{
+		Addr:        "40.125.164.122:10053",
+		Name:        "fabric2.3",
+		OrganizationsPath: ".",
+		Username:    "Admin",
+		CCID:        "Broker-001",
+		ChannelId:   "mychannel",
+	}
+}
+
+func UnmarshalConfig(configPath string) (*Fabric, error) {
+	viper.SetConfigFile(filepath.Join(configPath, ConfigName))
+	viper.SetConfigType("toml")
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("FABRIC")
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	config := DefaultConfig()
+
+	if err := viper.Unmarshal(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
--- client.go
+++ client.go
@@ -129,42 +129,7 @@ func NewClient(configPath, pierId string, extra []byte) (client.Client, error) {
 	}, nil
 }
 
-func populateWallet(wallet *gateway.Wallet, organizationsPath string, username string) error {
-	credPath := filepath.Join(
-		organizationsPath,
-		"peerOrganizations",
-		"org1.example.com",
-		"users",
-		"User1@org1.example.com",
-		"msp",
-	)
 
-	certPath := filepath.Join(credPath, "signcerts", "User1@org1.example.com-cert.pem")
-	// read the certificate pem
-	cert, err := ioutil.ReadFile(filepath.Clean(certPath))
-	if err != nil {
-		return err
-	}
-
-	keyDir := filepath.Join(credPath, "keystore")
-	// there's a single file in this dir containing the private key
-	files, err := ioutil.ReadDir(keyDir)
-	if err != nil {
-		return err
-	}
-	if len(files) != 1 {
-		return fmt.Errorf("keystore folder should have contain one file")
-	}
-	keyPath := filepath.Join(keyDir, files[0].Name())
-	key, err := ioutil.ReadFile(filepath.Clean(keyPath))
-	if err != nil {
-		return err
-	}
-
-	identity := gateway.NewX509Identity("Org1MSP", string(cert), string(key))
-
-	return wallet.Put(username, identity)
-}
 
 func (c *Client) Start() error {
 	logger.Info("Fabric consumer started")
